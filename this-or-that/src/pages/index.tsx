b9c28668c517599e8e5d8290a74dbf77eb05de9e
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-10 10:49:15 -0500

    basic ui

--- this-or-that/src/pages/index.tsx
+++ this-or-that/src/pages/index.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-2xl'>hello world</h1>
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center'>Which is rounder?</div>
+      <div className='p-2' />
+      <div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
+        <div className='w-16 h-16 bg-red-200' />
+        <div className='p-8'>vs</div>
+        <div className='w-16 h-16 bg-red-200' />
+      </div>
     </div>
   );
 }
