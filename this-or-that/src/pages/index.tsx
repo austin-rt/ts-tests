63efd4609f2b28e4ed12d5749d8b74725c54a875
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-10 10:33:15 -0500

    set default css globals

--- this-or-that/src/pages/index.tsx
+++ this-or-that/src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <div>
-      <h1 className='text-2xl text-red-500'>hello</h1>
+      <h1 className='text-2xl'>hello</h1>
     </div>
   );
 }
