1155d0c1dac31bdc6ac3a3c2843ff83ab06b3a46
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-10 13:03:35 -0500

    assign value and onchange

--- notes/src/components/NoteForm.tsx
+++ notes/src/components/NoteForm.tsx
@@ -8,6 +8,7 @@ import { Tag } from '../models/types';
 const NoteForm = ({ onSubmit }: NoteFormProps) => {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -34,7 +35,19 @@ const NoteForm = ({ onSubmit }: NoteFormProps) => {
           <Col>
             <Form.Group controlId='tags'>
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect isMulti />
+              <CreatableReactSelect
+                value={selectedTags.map(tag => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={tags => {
+                  setSelectedTags(
+                    tags.map(tag => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
