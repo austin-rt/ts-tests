2e0661a944763296e3380633da221f7253415649
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-10 12:08:41 -0500

    config react router

--- notes/src/App.tsx
+++ notes/src/App.tsx
@@ -4,7 +4,38 @@ import { Container } from 'react-bootstrap';
 import NewNote from './components/NewNote';
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Container className='my-4'>
+      <Routes>
+        <Route
+          path='/'
+          element={<h1>Home</h1>}
+        />
+        <Route
+          path='/new'
+          element={<NewNote />}
+        />
+        <Route path=':id'>
+          <Route
+            index
+            element={<h1>Show</h1>}
+          />
+          <Route
+            path='edit'
+            element={<h1>Edit</h1>}
+          />
+        </Route>
+        <Route
+          path=' '
+          element={<Navigate to='/' />}
+        />
+        <Route
+          path='*'
+          element={<Navigate to='/' />}
+        />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
