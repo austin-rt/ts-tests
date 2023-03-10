813a8c86247c4646dd8b595307b83027229c93b4
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-10 12:08:13 -0500

    gut app

---
+++ notes/src/App.tsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import NewNote from './components/NewNote';
+
+function App() {
+  return <h1>hello</h1>;
+}
+
+export default App;
