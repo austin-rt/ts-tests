2e60189bcf6b0ec82b46f85588a6aa5d59f1c995
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-13 15:56:08 -0400

    testing sign in

--- recipes/src/pages/_app.tsx
+++ recipes/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, SignedIn, UserButton } from '@clerk/nextjs';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <Component {...pageProps} />
     </ClerkProvider>
   );
