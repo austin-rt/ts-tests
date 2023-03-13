7f05b49f9c7708f3e1fcbdb1cf9b6dc7c1159723
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-13 10:48:59 -0400

    import and use React Markdown

--- notes/src/components/Note.tsx
+++ notes/src/components/Note.tsx
@@ -1,13 +1,14 @@
 import { Badge, Button, Col, Row, Stack } from 'react-bootstrap';
 import { useNote } from './NoteLayout';
 import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 const Note = () => {
   const note = useNote();
 
   return (
     <>
-      <Row classNmae='align-items-center mb-4'>
+      <Row className='align-items-center mb-4'>
         <Col>
           <h1>{note.title}</h1>
           {note.tags.length && (
@@ -34,19 +35,15 @@ const Note = () => {
           >
             <Link to={`/${note.id}/edit`}>
               <Button variant='primary'>Edit</Button>
-              <Button
-                variant='outline-danger'
-                className='mx-2'
-              >
-                Delete
-              </Button>
-              <Link to='..'>
-                <Button variant='outline-secondary'>Back</Button>
-              </Link>
+            </Link>
+            <Button variant='outline-danger'>Delete</Button>
+            <Link to='..'>
+              <Button variant='outline-secondary'>Back</Button>
             </Link>
           </Stack>
         </Col>
       </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
   );
 };
